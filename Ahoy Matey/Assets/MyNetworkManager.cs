ba02bf1f64c5dd43ce25bfd70ca7b4488b5c2e7f
Daniel Kunicki <dankunicki@gmail.com>
2018-04-09 23:18:41 +0200

    Host-client example

---
+++ Ahoy Matey/Assets/MyNetworkManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager
+{
+
+    public void MyStartHost()
+    {
+        StartHost();
+        Debug.Log(Time.timeSinceLevelLoad + "Starting host.");
+    }
+
+    public override void OnStartHost()
+    {
+        Debug.Log(Time.timeSinceLevelLoad + "Host started.");
+    }
+
+    public override void OnStartClient(NetworkClient client)
+    {
+        Debug.Log(Time.timeSinceLevelLoad + "Client started.");
+        InvokeRepeating("PrintDot", 0f, 1f);
+    }
+
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        Debug.Log(Time.timeSinceLevelLoad + "Client connected to " + conn.address);
+        if (IsInvoking("PrintDot")) CancelInvoke("PrintDot");
+    }
+
+    void PrintDot()
+    {
+        Debug.Log(".");
+    }
+
+    
+}

9a5ecdb9ad3f00beb21906ade556301030cc9aff
Daniel Kunicki <dankunicki@gmail.com>
2018-04-14 17:24:56 +0200

    * Add CrossPlatformInput * Add player movement * Add player camera * Setup network behaviour for players

---
+++ Ahoy Matey/Assets/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour
+{
+
+    public float speed = 5f;
+    Vector3 shift;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isLocalPlayer)
+        {
+            shift.x = CrossPlatformInputManager.GetAxis("Horizontal");
+            shift.z = CrossPlatformInputManager.GetAxis("Vertical");
+
+            transform.position += Time.deltaTime * speed * shift;
+        }
+
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        base.OnStartLocalPlayer();
+        if (isLocalPlayer)
+        {
+            Camera myCamera = GetComponentInChildren<Camera>();
+            myCamera.enabled = true;
+        }
+
+    }
+}
